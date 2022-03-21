730fcbf4cfb973e3021df52205b51c31dcaa3ca4
mhmdalaa-sim <80651664+mhmdalaa-sim@users.noreply.github.com>
2022-03-21 22:59:00 +0200

    Add files via upload

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -8,16 +8,25 @@ import RefundsDashboard from "./pages/Examination Comitee/RefundsDashboard"
 import TestData from "./data/TestData"
 import TableTemp from "./components/TableTemp"
 import TableData from "./data/TableData"
+import WarehousesData from "./data/WarehousesData"
 import RefundTable from "./pages/Examination Comitee/RefundTable"
 import Item from "./pages/Examination Comitee/Item"
 import MainControl from "./pages/Examination Comitee/MainControl"
 import AdminNavBar from "./pages/Admin/AdminNavBar"
+import LIstOfItems from "./pages/Admin/LIstOfItems"
+import itemsList from "./data/itemsList"
+import WareHousesDashboard from "./pages/Admin/WareHousesDashboard"
+import NewItem from "./components/NewItem"
+import Wtest from "./components/Wtest"
+
 
 function App(){
     
     const[Data ,setData] =useState(TestData)
     const[td ,settd] =useState(TableData)
-   
+    const[il,setil]=useState(itemsList)
+    const[WHD,setWHD]=useState(WarehousesData)
+    
    
 
     return (
@@ -64,10 +73,33 @@ function App(){
         
                                                         //this is Admin routes
              
-        <Route path="/AdminNav" element={
+        <Route path="/Admin" element={
          <> <AdminNavBar></AdminNavBar> </>
             
         }></Route>
+
+        <Route path="/itemslist" element={
+            <LIstOfItems aylist={il}></LIstOfItems>
+        }></Route>
+
+        <Route path="/Admin/NewItem" element={
+
+            <NewItem></NewItem>
+        }></Route>
+
+        <Route path="WareHouses" element={
+
+            <WareHousesDashboard WH={WHD}></WareHousesDashboard>
+        }></Route>
+
+
+        <Route path="/WareHouses/:id" element={
+        
+        <Wtest></Wtest>    
+        }></Route>
+
+
+        
           </Routes>
       </Router>
     
